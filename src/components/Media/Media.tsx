3b5d1f9e91b3ff79287ea5041fcaa2e7bd4d492f
Sam McMurray <scmcmurr@gmail.com>
2023-02-03 00:09:43 -0500

    Added some icons

--- src/components/Media/Media.tsx
+++ src/components/Media/Media.tsx
@@ -1,5 +1,8 @@
 import { FC, useMemo, useState } from "react";
 import { Paper } from '@mui/material';
+import Icon from '@mdi/react';
+import { mdiSpotify, mdiTimer  } from '@mdi/js';
+
 
 import TimerBlob from './Timer/TimerBlob';
 import Spotify from './Spotify/Spotify';
@@ -13,9 +16,13 @@ const Media: FC<{}> = () => {
     <Box height="100%" component="div">
       <Paper style={{ height: "50%" }} >
         <Box display="flex" height="100%">
-          <Box width="50px">
-            <Box>S</Box>
-            <Box>T</Box>
+          <Box width="50px" border={1}>
+            <Box display="flex" alignItems="center" justifyContent="center" height="50%">
+              <Icon path={mdiSpotify} size={1} />
+            </Box>
+            <Box display="flex" alignItems="center" justifyContent="center" height="50%">
+              <Icon path={mdiTimer } size={1} />
+            </Box>
           </Box>
           <Box flexGrow={1}>
             <TimerBlob time={timeInput} />
